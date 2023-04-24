5e043130fd7ddd709f46cb266f6e79b62516d932
saad ouchama <saadochama@gmail.com>
2023-04-24 16:41:57 +0000

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * by Saad and Charaf
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
