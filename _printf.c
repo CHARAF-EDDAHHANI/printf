db6621ce57f17967de64f7420f09b026af469fe3
saad ouchama <saadochama@gmail.com>
2023-04-24 20:38:08 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ * by Joseph and Ciny
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -38,7 +39,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					       flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
