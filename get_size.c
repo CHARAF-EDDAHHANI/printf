5701dfcc5b8fada8a025c221d8d1ba7346dd5ee1
saad ouchama <saadochama@gmail.com>
2023-04-24 16:41:07 +0000

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * get_size -Calculates the size to cast the argument
- * by Saad and Charaf
+ * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
