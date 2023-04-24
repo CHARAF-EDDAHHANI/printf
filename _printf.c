eaa307761271521a583c895da559f1780e5945d6
saad ouchama <saadochama@gmail.com>
2023-04-24 16:04:40 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * by Joseph and Ciny
+ * by Saad and Charaf
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
