d2efb321dbc159d3be12a2ccae1d5397af077a70
saad ouchama <saadochama@gmail.com>
2023-04-24 16:06:13 +0000

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -3,6 +3,7 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
+ * by Saad and Charaf
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
