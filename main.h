38de5e312d56f5c1ef62e09fca148ab579a1cadb
saad ouchama <saadochama@gmail.com>
2023-04-24 20:17:14 +0000

    Update main.h

--- main.h
+++ main.h
@@ -21,8 +21,8 @@
 /**
  * struct fmt - Struct op
  *
- * @fmt: The format.
- * @fn: The function associated.
+ * @fmt: The format
+ * @fn: The function associated
  */
 struct fmt
 {
@@ -34,8 +34,8 @@ struct fmt
 /**
  * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: The format.
- * @fm_t: The function associated.
+ * @fmt: The format
+ * @fm_t: The function associated
  */
 typedef struct fmt fmt_t;
 
