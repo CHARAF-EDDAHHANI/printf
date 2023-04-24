15d24b6c9058969315bd426dd8c92f6a15a7ce21
saad ouchama <saadochama@gmail.com>
2023-04-24 16:41:31 +0000

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -2,7 +2,6 @@
 
 /**
  * get_width - Calculates the width for printing
- * by Saad and Charaf
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
